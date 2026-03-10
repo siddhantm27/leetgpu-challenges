1c012f9632c5330bd3db092afa6e25897a650678
siddhantm27 <smulkikar@gmail.com>
2026-03-11 00:53:11 +0530

    check mat mul

--- challenges/easy/2_matrix_multiplication/starter/starter.cu
+++ challenges/easy/2_matrix_multiplication/starter/starter.cu
@@ -1,7 +1,18 @@
 #include <cuda_runtime.h>
 
 __global__ void matrix_multiplication_kernel(const float* A, const float* B, float* C, int M, int N,
-                                             int K) {}
+                                             int K) {
+        int idx = blockDim.x*blockIdx.x + threadIdx.x;
+        int idy = blockDim.y*blockIdx.y + threadIdx.y;
+        if (idy < M && idx < K)
+        {
+            C[idy*M + idx] = 0;
+            for (int i = 0;i<N;i++){
+                    C[idy*K + idx] += A[idy*N + i]*B[i*K + idx];
+            }
+        }
+        
+    }
 
 // A, B, C are device pointers (i.e. pointers to memory on the GPU)
 extern "C" void solve(const float* A, const float* B, float* C, int M, int N, int K) {
