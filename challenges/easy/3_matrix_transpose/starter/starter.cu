1d0196ef581ed12607b2611fa4e71666a7c6800d
siddhantm27 <smulkikar@gmail.com>
2026-03-11 01:02:20 +0530

    easy-3

--- challenges/easy/3_matrix_transpose/starter/starter.cu
+++ challenges/easy/3_matrix_transpose/starter/starter.cu
@@ -1,6 +1,13 @@
 #include <cuda_runtime.h>
 
-__global__ void matrix_transpose_kernel(const float* input, float* output, int rows, int cols) {}
+__global__ void matrix_transpose_kernel(const float* input, float* output, int rows, int cols) {
+    int col =blockIdx.x*blockDim.x + threadIdx.x;
+    int row = blockIdx.y*blockDim.y + threadIdx.y;
+
+    if (col < cols && row < rows){
+        output[col*rows + row] = input[row*cols + col];
+    }
+}
 
 // input, output are device pointers (i.e. pointers to memory on the GPU)
 extern "C" void solve(const float* input, float* output, int rows, int cols) {
