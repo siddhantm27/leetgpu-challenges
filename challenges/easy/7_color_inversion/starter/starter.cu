3bed8daefba2d148a37b32071a363974102f21af
siddhantm27 <smulkikar@gmail.com>
2026-03-11 10:19:20 +0530

    easy-7 converted to float

--- challenges/easy/7_color_inversion/starter/starter.cu
+++ challenges/easy/7_color_inversion/starter/starter.cu
@@ -1,16 +1,17 @@
 #include <cuda_runtime.h>
 #include <iostream>
 
-__global__ void invert_kernel(unsigned char* image, int width, int height) {
+__global__ void invert_kernel(float* image, int width, int height) {
 
     int idx = blockDim.x * blockIdx.x + threadIdx.x;
 
     if (idx < width * height && idx % 4 != 3) {
-        image[idx] = 255 - image[idx];
+        image[idx] = 255.0f - image[idx];
     }
 }
-// image_input, image_output are device pointers (i.e. pointers to memory on the GPU)
-extern "C" void solve(unsigned char* image, int width, int height) {
+
+extern "C" void solve(float* image, int width, int height) {
+
     int threadsPerBlock = 256;
     int blocksPerGrid = (width * height + threadsPerBlock - 1) / threadsPerBlock;
 
