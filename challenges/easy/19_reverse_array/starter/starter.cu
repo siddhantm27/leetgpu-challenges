9717878160d707735d6b40c05403526204731d07
siddhantm27 <smulkikar@gmail.com>
2026-03-11 11:02:07 +0530

    easy-19

--- challenges/easy/19_reverse_array/starter/starter.cu
+++ challenges/easy/19_reverse_array/starter/starter.cu
@@ -1,6 +1,14 @@
 #include <cuda_runtime.h>
 
-__global__ void reverse_array(float* input, int N) {}
+__global__ void reverse_array(float* input, int N) {
+    int id = blockDim.x*blockIdx.x + threadIdx.x;
+
+    if (id < (N+1)/2){
+        float temp = input[id];
+        input[id] = input[N - id -1];
+        input[N-id-1] = temp;
+    }
+}
 
 // input is device pointer
 extern "C" void solve(float* input, int N) {
