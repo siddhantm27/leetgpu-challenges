a8fbd753f05e2e6a528f51ae93e2d23ed64c0d8a
siddhantm27 <smulkikar@gmail.com>
2026-03-11 10:55:50 +0530

    easy-9

--- challenges/easy/9_1d_convolution/starter/starter.cu
+++ challenges/easy/9_1d_convolution/starter/starter.cu
@@ -1,7 +1,17 @@
 #include <cuda_runtime.h>
 
 __global__ void convolution_1d_kernel(const float* input, const float* kernel, float* output,
-                                      int input_size, int kernel_size) {}
+                                      int input_size, int kernel_size) {
+                                        int id = blockDim.x*blockIdx.x + threadIdx.x;
+
+                                        if (id + kernel_size <= input_size){
+                                            output[id] = 0;
+                                            for (int i = 0;i<kernel_size;i++)
+                                            {
+                                                output[id]+= input[id+i]*kernel[i];
+                                            }
+                                        }
+                                      }
 
 // input, kernel, output are device pointers (i.e. pointers to memory on the GPU)
 extern "C" void solve(const float* input, const float* kernel, float* output, int input_size,
