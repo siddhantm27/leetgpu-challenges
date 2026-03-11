354af4f8d3e96b52cf8790c49631857b6f34ec6e
siddhantm27 <smulkikar@gmail.com>
2026-03-11 10:07:05 +0530

    easy-7

--- challenges/easy/7_color_inversion/starter/starter.cu
+++ challenges/easy/7_color_inversion/starter/starter.cu
@@ -1,6 +1,13 @@
 #include <cuda_runtime.h>
 
-__global__ void invert_kernel(unsigned char* image, int width, int height) {}
+__global__ void invert_kernel(unsigned char* image, int width, int height) {
+    int col = blockDim.x*blockIdx.x + threadIdx.x;
+    int row = blockDim.y*blockIdx.y + threadIdx.y;
+
+    if (row < height && col < width){
+        image[row*width + col] = 255 - image[row*width + col];
+    }
+}
 // image_input, image_output are device pointers (i.e. pointers to memory on the GPU)
 extern "C" void solve(unsigned char* image, int width, int height) {
     int threadsPerBlock = 256;
